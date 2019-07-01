b4d8d0929c01a5d45b1600b76e9b8b0c6ca38f04
huoyun <809590403@qq.com>
2019-07-01 21:33:51 +0800

    使用mybatis-generator-gui生成entity和mapper接口以及mapper.xml 引入page-helper

--- src/main/java/com/huo/msb/java/hellospringboot/account/service/AccountService.java
+++ src/main/java/com/huo/msb/java/hellospringboot/account/service/AccountService.java
@@ -1,10 +1,10 @@
 package com.huo.msb.java.hellospringboot.account.service;
 
+import com.github.pagehelper.PageHelper;
 import com.huo.msb.java.hellospringboot.account.entity.Account;
-import com.huo.msb.java.hellospringboot.account.mapper.AccountMapper;
+import com.huo.msb.java.hellospringboot.account.entity.AccountExample;
+import com.huo.msb.java.hellospringboot.account.mapper.AccountDAO;
 import com.huo.msb.java.hellospringboot.account.resp.RespStat;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +19,12 @@ import java.util.List;
 public class AccountService {
 
     @Autowired
-    private AccountMapper mapper;
+    private AccountDAO accountDAO;
 
     public RespStat save(Account account) {
         Integer result = null;
         try {
-            result = mapper.save(account);
+            result = accountDAO.insert(account);
         } catch (Exception e) {
             e.printStackTrace();
             return RespStat.build(500, e.getMessage(), null);
@@ -34,15 +34,27 @@ public class AccountService {
     }
 
 
-    public RespStat findAll() {
-        List<Account> all = mapper.findAll();
+    public RespStat findAll(Integer page, Integer size) {
+        AccountExample accountExample = new AccountExample();
+        List<Account> all = null;
+        if (page == null && size == null) {
+            // 默认就是查所有
+//        accountExample.createCriteria().;
+            all = accountDAO.selectByExample(accountExample);
+            return RespStat.build(200, "success", all);
+        }
+
+        // 分页查询
+        PageHelper.startPage(page, size);
+        all = accountDAO.selectByExample(accountExample);
         return RespStat.build(200, "success", all);
+
     }
 
     public RespStat deleteById(Integer id) {
         Integer result = null;
         try {
-            result = mapper.deleteById(id);
+            result = accountDAO.deleteByPrimaryKey(id);
         } catch (Exception e) {
             return RespStat.build(500, e.getMessage(), null);
         }
@@ -52,7 +64,7 @@ public class AccountService {
     public RespStat findById(Integer id) {
         Account result = null;
         try {
-            result = mapper.findById(id);
+            result = accountDAO.selectByPrimaryKey(id);
         } catch (Exception e) {
             return RespStat.build(500, e.getMessage(), result);
 
@@ -61,20 +73,28 @@ public class AccountService {
 
     }
 
-    public RespStat editById(Account account) {
-        Integer result = null;
-        try {
-            result = mapper.updateById(account);
-        } catch (Exception e) {
-            return RespStat.build(500, e.getMessage(), result);
-        }
-        return RespStat.build(200, "ok", result);
-    }
-
+    //
+//    public RespStat editById(Account account) {
+//        Integer result = null;
+//        try {
+//            result = mapper.updateById(account);
+//        } catch (Exception e) {
+//            return RespStat.build(500, e.getMessage(), result);
+//        }
+//        return RespStat.build(200, "ok", result);
+//    }
+//
     public RespStat login(Account account) {
         Account result = null;
+        AccountExample accountExample = new AccountExample();
+        accountExample.createCriteria().andLoginNameEqualTo(account.getLoginName()).andPasswordEqualTo(account.getPassword());
         try {
-            result = mapper.findByLoginNameAndPassword(account);
+            List<Account> accounts = accountDAO.selectByExample(accountExample);
+            if (accounts.size() == 1) {
+                result = accounts.get(0);
+            } else {
+                return RespStat.build(401, "用户名密码错误", result);
+            }
         } catch (Exception e) {
             return RespStat.build(500, e.getMessage(), result);
 
